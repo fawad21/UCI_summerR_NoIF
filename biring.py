de7d7d39e793d76146797ed3a4a9173ef975d1bd
fawad21 <alilegend213@gmail.com>
2025-07-07 10:08:51 -0700

    biring.py
    clockwise bidrectional ring topology

---
+++ biring.py
@@ -0,0 +1,36 @@
+"""
+biring.py – simple clockwise bidrectional ring topology for Ruby/Garnet
+refrence:https://gem5bootcamp.github.io/2024/#03-Developing-gem5-models/08-ruby-network
+
+"""
+
+from topologies.BaseTopology import BaseTopology
+from m5.objects import *
+
+
+class biring(BaseTopology):
+    description=("clockwise bidirectional ring")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
