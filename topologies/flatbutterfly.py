665878b1272ad1bcae2fb3f11c6feae70a336ed6
fawad21 <alilegend213@gmail.com>
2025-07-09 18:00:59 -0700

    Update flatbutterfly.py

--- topologies/flatbutterfly.py
+++ topologies/flatbutterfly.py
@@ -6,3 +6,18 @@ Refrence: https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=4408254&tag=1
 """
 
 
+from topologies.BaseTopology import BaseTopology
+from m5.objects import *
+
+class flattenedbutterfly(BaseTopology):
+    description = "2d flattened Butterfly Topology"
+
+    def __init__ (self, controllers):
+        super().__init__()
+        self.nodes = controllers
+    
+    def makeTopology(self, opts, net, IntLink, ExtLink, Router):
+        link_lat = opts.link_latency
+        router_lat = opts.router_latency
+
+        
